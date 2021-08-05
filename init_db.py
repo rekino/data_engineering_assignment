47e65551e33e91e29fc55b7ba0453b87354abbce
Ramin Barati <rekino@gmail.com>
2021-08-05 14:04:42 +0000

    initialize the database

---
+++ init_db.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+print('Begin migration...')
+
+conn = psycopg2.connect(database="postgres", user = "postgres", password = "postgres", host = "db", port = "5432")
+print('Opened database successfully')
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE impressions
+      (banner_id    INT,
+       campaign_id  INT,
+       PRIMARY KEY (banner_id,campaign_id));''')
+
+cur.execute('''CREATE TABLE clicks
+      (click_id    INT PRIMARY KEY,
+       banner_id    INT NOT NULL,
+       campaign_id  INT NOT NULL,
+       FOREIGN KEY (banner_id,campaign_id) REFERENCES impressions (banner_id,campaign_id));''')
+
+cur.execute('''CREATE TABLE conversions
+      (conversion_id    INT PRIMARY KEY,
+       click_id         INT NOT NULL,
+       revenue          INT NOT NULL,
+       FOREIGN KEY (click_id) REFERENCES clicks (click_id));''')
+
+print('Tables created successfully')
+
+conn.commit()
+conn.close()
