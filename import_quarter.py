4348dcb34c9e310a417f66201a1b72d947e9ced7
Ramin Barati <rekino@gmail.com>
2021-08-05 17:26:07 +0000

    import csv files

--- import_quarter.py
+++ import_quarter.py
@@ -3,14 +3,95 @@ import sys, getopt
 import logging
 import csv
 
-def import_impressions(cur, file, quarter):
-    pass
+def import_impressions(conn, file, quarter):
+    cur = conn.cursor()
+    reader = csv.reader(file)
+    reader.__next__() # ignoring the first line
+    count = 0
+    for i, row in enumerate(reader):
+        if i % 100 == 0:
+            conn.commit()
+        try:
+            cur.execute(f'''SELECT 1 FROM impressions WHERE banner_id={row[0]} AND campaign_id={row[1]}''')
+            query = cur.fetchall()
 
-def import_clicks(cur, file, quarter):
-    pass
+            duplicate = False
+            for _ in query:
+                duplicate = True
+                break
 
-def import_conversions(cur, file, quarter):
-    pass
+            if duplicate:
+                logging.warning(f'duplicate impression in {file.name} line {i+2}: ({row[0]}, {row[1]})')
+                continue
+
+            cur.execute(f'''INSERT INTO impressions (banner_id,campaign_id) VALUES ({row[0]},{row[1]})''')
+
+            count += 1
+        except Exception as error:
+            logging.error(f'error in importing impression in {file.name} line {i+2}: {error}')
+            conn.commit()
+    conn.commit()
+    print(f'imported {count} of {i+1} impressions')
+
+def import_clicks(conn, file, quarter):
+    cur = conn.cursor()
+    reader = csv.reader(file)
+    reader.__next__() # ignoring the first line
+    count = 0
+    for i, row in enumerate(reader):
+        if i % 100 == 0:
+            conn.commit()
+        try:
+            cur.execute(f'''SELECT 1 FROM clicks WHERE click_id={row[0]}''')
+            query = cur.fetchall()
+
+            duplicate = False
+            for _ in query:
+                duplicate = True
+                break
+
+            if duplicate:
+                logging.warning(f'duplicate click in {file.name} line {i+2}: ({row[0]}, {row[1]}, {row[2]})')
+                continue
+
+            cur.execute(f'''INSERT INTO clicks (click_id,banner_id,campaign_id,quarter) VALUES ({row[0]},{row[1]},{row[2]},{quarter})''')
+
+            count += 1
+        except Exception as error:
+            logging.error(f'error in importing click in {file.name} line {i+2}: {error}')
+            conn.commit()
+    conn.commit()
+    print(f'imported {count} of {i+1} clicks')
+
+def import_conversions(conn, file, quarter):
+    cur = conn.cursor()
+    reader = csv.reader(file)
+    reader.__next__() # ignoring the first line
+    count = 0
+    for i, row in enumerate(reader):
+        if i % 100 == 0:
+            conn.commit()
+        try:
+            cur.execute(f'''SELECT 1 FROM conversions WHERE conversion_id={row[0]}''')
+            query = cur.fetchall()
+
+            duplicate = False
+            for _ in query:
+                duplicate = True
+                break
+
+            if duplicate:
+                logging.warning(f'duplicate conversion in {file.name} line {i+2}: ({row[0]}, {row[1]}, {row[2]})')
+                continue
+
+            cur.execute(f'''INSERT INTO conversions (conversion_id,click_id,revenue) VALUES ({row[0]},{row[1]},{row[2]})''')
+
+            count += 1
+        except Exception as error:
+            logging.error(f'error in importing conversion in {file.name} line {i+2}: {error}')
+            conn.commit()
+    conn.commit()
+    print(f'imported {count} of {i+1} conversions')
 
 def main(argv):
     folder = ''
@@ -39,18 +120,15 @@ def main(argv):
 
     conn = psycopg2.connect(database="postgres", user="postgres", password="postgres", host="db", port="5432")
 
-    cur = conn.cursor()
-
     with open(f'{folder}/impressions_{quarter}.csv') as impressions:
-        import_impressions(cur, impressions, quarter)
+        import_impressions(conn, impressions, quarter)
 
     with open(f'{folder}/clicks_{quarter}.csv') as clicks:
-        import_clicks(cur, clicks, quarter)
+        import_clicks(conn, clicks, quarter)
 
     with open(f'{folder}/conversions_{quarter}.csv') as conversions:
-        import_conversions(cur, conversions, quarter)
+        import_conversions(conn, conversions, quarter)
 
-    conn.commit()
     conn.close()
 
 
